4f581a199912fd5073ed3c326713ddadac8c242e
Umut Enes Soğukpınar <usogukpinar0@gmail.com>
2025-01-22 23:38:04 +0300

    main part finished I guess...

--- srcs/process.c
+++ srcs/process.c
@@ -6,7 +6,7 @@
 /*   By: umut <umut@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 22:05:35 by umut              #+#    #+#             */
-/*   Updated: 2025/01/22 21:11:30 by umut             ###   ########.fr       */
+/*   Updated: 2025/01/22 23:26:50 by umut             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,32 @@
 
 void	child(t_pipex *pipex, char *infile, char **envp)
 {
-	int fd_c;
+	int infile_fd;
 
-	fd_c = open(infile, O_RDONLY, 0777);
-	if (fd_c < 0)
+	infile_fd = open(infile, O_RDONLY);
+	if (infile_fd < 0)
 		shut_program_error(pipex, NULL);
-	if (dup2(fd_c, STDIN_FILENO) < 0)
+	if (dup2(infile_fd, STDIN_FILENO) < 0)
 		shut_program_default(pipex, NULL);
 	if (dup2((((pipex->opt_list)[0])->fd)[1], STDOUT_FILENO) < 0)
 		shut_program_error(pipex, NULL);
 	close((((pipex->opt_list)[0])->fd)[0]);
+	close(infile_fd);
 	execute(pipex, (pipex->opt_list)[0], envp);
 }
 
 void	parent(t_pipex *pipex, char *outfile, char **envp)
 {
-	int	fd_p;
+	int	outfile_fd;
 
-	fd_p = open(outfile, O_CREAT | O_TRUNC | O_WRONLY, 0777);
-	if (fd_p < 0)
+	outfile_fd = open(outfile, O_CREAT | O_TRUNC | O_WRONLY, 0777);
+	if (outfile_fd < 0)
 		shut_program_error(pipex, NULL);
-	if (dup2((((pipex->opt_list)[0])->fd)[1], STDIN_FILENO) < 0)
+	if (dup2((((pipex->opt_list)[0])->fd)[0], STDIN_FILENO) < 0)
 		shut_program_error(pipex, NULL);
-	if (dup2(fd_p, STDOUT_FILENO) < 0)
+	if (dup2(outfile_fd, STDOUT_FILENO) < 0)
 		shut_program_error(pipex, NULL);
 	close((((pipex->opt_list)[0])->fd)[1]);
+	close(outfile_fd);
 	execute(pipex, (pipex->opt_list)[1], envp);
 }
